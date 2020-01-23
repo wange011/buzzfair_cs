c796a43aae87cc748e9b3602243d21b9a57c7851
wange011 <wange011@gmail.com>
2020-01-22 20:29:50 -0500

    Updates

---
+++ precache-manifest.369b6c0d7ac70be24b4f883f798d988f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314f7d48734a9cf09f579c731a976529",
+    "url": "/buzzfair_cs/index.html"
+  },
+  {
+    "revision": "6fc6915bd78d999c9151",
+    "url": "/buzzfair_cs/static/css/main.48360f49.chunk.css"
+  },
+  {
+    "revision": "e63b90a3b640984e4fb1",
+    "url": "/buzzfair_cs/static/js/2.a31c9053.chunk.js"
+  },
+  {
+    "revision": "6fc6915bd78d999c9151",
+    "url": "/buzzfair_cs/static/js/main.db4bbec7.chunk.js"
+  },
+  {
+    "revision": "c277adf91cd0c714a4e0",
+    "url": "/buzzfair_cs/static/js/runtime~main.d091f292.js"
+  }
+]);
